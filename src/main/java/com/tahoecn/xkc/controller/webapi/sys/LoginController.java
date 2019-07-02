d0a1ae6b2982653bf764904632407e8cb8fdc0cf
yinliming <jianxia52123@163.com>
2019-07-02 16:58:09 +0800

    登陆优化

--- src/main/java/com/tahoecn/xkc/controller/webapi/sys/LoginController.java
+++ src/main/java/com/tahoecn/xkc/controller/webapi/sys/LoginController.java
@@ -2,6 +2,7 @@ package com.tahoecn.xkc.controller.webapi.sys;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.landray.sso.client.EKPSSOContext;
 import com.landray.sso.client.oracle.CookieUtil;
 import com.tahoecn.core.json.JSONResult;
@@ -72,50 +73,50 @@ public class LoginController extends TahoeBaseController {
             return markError("用户不存在");
         }
 
-        HashMap<String,String> userJob = accountService.getUserJob(userName);
+        //不需要HashMap<String,String> userJob = accountService.getUserJob(userName);
         List<HashMap<String,String>> userProduct = accountService.getUserPorduct(userName);
         if (userProduct == null){
             return markError("账号无权限登录系统");
         }else{
             String productId = userProduct.get(0).get("ID");
 
-            HashMap<String,String> userProject = accountService.getUserPorject(userJob.get("UserID"),productId);
+            //不需要HashMap<String,String> userProject = accountService.getUserPorject(userJob.get("UserID"),productId);
             List<HashMap<String,String>> userJobMenus = accountService.getUserJobMenus(userName,productId);
             if (userJobMenus == null){
                 return markError("账号无权限登录系统");
             }
-            List<HashMap<String,String>> userWXApp = accountService.getUserWXApp(userName);
+            //不需要List<HashMap<String,String>> userWXApp = accountService.getUserWXApp(userName);
             List<HashMap<String,String>> userJobs = accountService.getUserJobs(userName,productId);
-            HashMap<String,String> currJob = userJobs.get(0);
-            List<HashMap<String,String>> jobFunctions = accountService.getJobFunctions(userName,productId);
+            //HashMap<String,String> currJob = userJobs.get(0);
+            //不需要List<HashMap<String,String>> jobFunctions = accountService.getJobFunctions(userName,productId);
 
             List<HashMap<String,String>> jobProject = accountService.getJobProject(userName);
 
-            List<HashMap<String,String>> userMenu = userJobMenus.stream().filter(j -> Objects.equals(j.get("JobID"), currJob.get("ID"))).collect(Collectors.toList());
-            List<HashMap<String,String>> userFunc = jobFunctions.stream().filter(j -> Objects.equals(j.get("JobID"), currJob.get("ID"))).collect(Collectors.toList());
-            String homeUrl = "";
+            //List<HashMap<String,String>> userMenu = userJobMenus.stream().filter(j -> Objects.equals(j.get("JobID"), currJob.get("ID"))).collect(Collectors.toList());
+
+            //不需要List<HashMap<String,String>> userFunc = jobFunctions.stream().filter(j -> Objects.equals(j.get("JobID"), currJob.get("ID"))).collect(Collectors.toList());
+            /*不需要String homeUrl = "";
             if (userMenu.stream().filter(c -> c.get("Url").equals(userProduct.get(0).get("Url"))).count() > 0){
                 homeUrl = userProduct.get(0).get("Url");
             }else{
                 homeUrl = userMenu.stream().filter(c -> StringUtils.isNotEmpty(c.get("Url"))).findFirst().get().get("Url");
-            }
-            System.out.println("homeUrl = " + homeUrl);
+            }*/
             HashMap<String,Object> result = new HashMap<>();
             result.put("User",account);
-            result.put("UserMenu",userMenu);
-            result.put("UserMenu2",userProject);
+            //result.put("UserMenu",userMenu);
+            /*result.put("UserMenu2",userProject);
             result.put("UserFunc",userFunc);
             result.put("UserProductProject",userProject);
-            result.put("UserProductWXApp",userWXApp);
+            result.put("UserProductWXApp",userWXApp);*/
 
-            result.put("UserProduct",userProduct);
-            result.put("CurrProduct",userProduct.get(0));
+            //result.put("UserProduct",userProduct);
+            //result.put("CurrProduct",userProduct.get(0));
             result.put("UserProductJob",userJobs);
-            result.put("CurrJob",currJob);
+            //result.put("CurrJob",currJob);
             result.put("UserProductJobMenu",userJobMenus);
-            result.put("UserProductJobFunc",jobFunctions);
+            //result.put("UserProductJobFunc",jobFunctions);
             result.put("JobProject",jobProject);
-            result.put("Url",homeUrl);
+            //result.put("Url",homeUrl);
             return markSuccess(result);
         }
 
