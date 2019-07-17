2e8c5cc403e5eb9affa290ca333ec67837f24087
WH <WH@DESKTOP-KV48RS0>
2019-07-17 10:47:05 +0800

    app -撞单客户列表bug

--- src/main/java/com/tahoecn/xkc/mapper/opportunity/BOpportunityMapper.java
+++ src/main/java/com/tahoecn/xkc/mapper/opportunity/BOpportunityMapper.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Map;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.tahoecn.xkc.model.opportunity.BOpportunity;
 
 /**
