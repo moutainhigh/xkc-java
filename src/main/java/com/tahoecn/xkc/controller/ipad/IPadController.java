f19d6321eb8ad4da3ad7cf8f9dc056156d2dd0f6
huqiuyang <839160103@qq.com>
2019-07-18 14:41:53 +0800

    Ipad

---
+++ src/main/java/com/tahoecn/xkc/controller/ipad/IPadController.java
@@ -0,0 +1,15 @@
+package com.tahoecn.xkc.controller.ipad;
+
+import io.swagger.annotations.Api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tahoecn.xkc.controller.TahoeBaseController;
+
+@RestController
+@Api(tags = "ipad-业务接口", value = "ipad-业务接口")
+@RequestMapping("/ipad/potenCust")
+public class IPadController extends TahoeBaseController{
+
+}
