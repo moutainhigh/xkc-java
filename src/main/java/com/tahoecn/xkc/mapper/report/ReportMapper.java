c258b61355c3afa55c153f5c60b67e838db20ca6
yinliming <jianxia52123@163.com>
2019-07-23 14:13:47 +0800

    new customer reserve init

---
+++ src/main/java/com/tahoecn/xkc/mapper/report/ReportMapper.java
@@ -0,0 +1,30 @@
+package com.tahoecn.xkc.mapper.report;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.tahoecn.xkc.model.channel.BChannelorg;
+import com.tahoecn.xkc.model.dto.ChannelDto;
+import com.tahoecn.xkc.model.dto.ChannelInsertDto;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author YYY
+ * @since 2019-06-25
+ */
+public interface ReportMapper extends BaseMapper<BChannelorg> {
+
+    List<Map<String,Object>> CustomerRankNew_Select(@Param("orglevel")String orglevel, @Param("accountID")String accountID,
+                                                    @Param("startDate")String startDate, @Param("endDate")String endDate, @Param("sql")String sql);
+
+    List<Map<String,Object>> CustomerRankSalesNew_Select(@Param("orgID")String orgID, @Param("startDate")String startDate,
+                                                         @Param("endDate")String endDate, @Param("sql")String sql);
+
+    List<Map<String,Object>> CustomerRankDetail_Select(@Param("orgID")String orgID, @Param("startDate")String startDate, @Param("endDate")String endDate);
+}
---
+++ src/main/java/com/tahoecn/xkc/service/report/impl/ReportServiceImpl.java
@@ -0,0 +1,39 @@
+package com.tahoecn.xkc.service.report.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tahoecn.xkc.mapper.channel.BChannelorgMapper;
+import com.tahoecn.xkc.mapper.report.ReportMapper;
+import com.tahoecn.xkc.model.channel.BChannelorg;
+import com.tahoecn.xkc.service.report.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author YYY
+ * @since 2019-06-25
+ */
+@Service
+public class ReportServiceImpl extends ServiceImpl<ReportMapper, BChannelorg> implements ReportService {
+
+    @Override
+    public List<Map<String, Object>> CustomerRankNew_Select(String orglevel, String accountID, String startDate, String endDate, String s) {
+        return baseMapper.CustomerRankNew_Select(orglevel, accountID, startDate, endDate, s);
+    }
+
+    @Override
+    public List<Map<String, Object>> CustomerRankSalesNew_Select(String orgID, String startDate, String endDate, String s) {
+        return baseMapper.CustomerRankSalesNew_Select(orgID, startDate, endDate, s);
+    }
+
+    @Override
+    public List<Map<String, Object>> CustomerRankDetail_Select(String orgID, String startDate, String endDate) {
+        return baseMapper.CustomerRankDetail_Select(orgID, startDate, endDate);
+    }
+}
