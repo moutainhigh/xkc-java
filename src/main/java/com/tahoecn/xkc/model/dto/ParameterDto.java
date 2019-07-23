c97f0ded5fbbf4a94449adb31002eeca122c246a
xuzhao <xuzhao@camelot.com>
2019-07-23 17:21:16 +0800

    gaara

---
+++ src/main/java/com/tahoecn/xkc/model/dto/ParameterDto.java
@@ -0,0 +1,51 @@
+package com.tahoecn.xkc.model.dto;
+
+
+import com.tahoecn.xkc.model.project.BProject;
+import com.tahoecn.xkc.model.rule.BRemindrule;
+import com.tahoecn.xkc.model.rule.BSalescenterrule;
+import java.io.Serializable;
+
+
+public class ParameterDto implements Serializable {
+
+    private String UserId;
+
+    private BProject bProject;
+
+    private BRemindrule bRemindrule;
+
+    private BSalescenterrule bSalescenterrule;
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+
+    public BProject getbProject() {
+        return bProject;
+    }
+
+    public void setbProject(BProject bProject) {
+        this.bProject = bProject;
+    }
+
+    public BRemindrule getbRemindrule() {
+        return bRemindrule;
+    }
+
+    public void setbRemindrule(BRemindrule bRemindrule) {
+        this.bRemindrule = bRemindrule;
+    }
+
+    public BSalescenterrule getbSalescenterrule() {
+        return bSalescenterrule;
+    }
+
+    public void setbSalescenterrule(BSalescenterrule bSalescenterrule) {
+        this.bSalescenterrule = bSalescenterrule;
+    }
+}
