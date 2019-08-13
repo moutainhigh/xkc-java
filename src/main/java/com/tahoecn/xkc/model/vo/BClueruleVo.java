9942ee74c1eef9af226ee46805b6c5f0218b895a
xuzhao <xuzhao@camelot.com>
2019-08-13 14:06:43 +0800

    保护期日志

--- src/main/java/com/tahoecn/xkc/model/vo/BClueruleVo.java
+++ src/main/java/com/tahoecn/xkc/model/vo/BClueruleVo.java
@@ -13,6 +13,16 @@ public class BClueruleVo implements Serializable {
 
     private String grouplist;
 
+    private String projectName;
+
+    private Integer isSelect;
+
+    private String oriProtectDays;
+
+    private String changeProtectDays;
+
+    private String userId;
+
     public BCluerule getbCluerule() {
         return bCluerule;
     }
@@ -35,5 +45,45 @@ public class BClueruleVo implements Serializable {
 
     public void setGrouplist(String grouplist) {
         this.grouplist = grouplist;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public Integer getIsSelect() {
+        return isSelect;
+    }
+
+    public void setIsSelect(Integer isSelect) {
+        this.isSelect = isSelect;
+    }
+
+    public String getOriProtectDays() {
+        return oriProtectDays;
+    }
+
+    public void setOriProtectDays(String oriProtectDays) {
+        this.oriProtectDays = oriProtectDays;
+    }
+
+    public String getChangeProtectDays() {
+        return changeProtectDays;
+    }
+
+    public void setChangeProtectDays(String changeProtectDays) {
+        this.changeProtectDays = changeProtectDays;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 }
---
+++ src/main/java/com/tahoecn/xkc/service/rule/IProtectConfLogService.java
@@ -0,0 +1,16 @@
+package com.tahoecn.xkc.service.rule;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.tahoecn.xkc.model.rule.ProtectConfLog;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author YYY
+ * @since 2019-08-13
+ */
+public interface IProtectConfLogService extends IService<ProtectConfLog> {
+
+}
