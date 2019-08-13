5669992e079c6d81e71d363a90c82e9a7c1e2ddb
WH <WH@DESKTOP-KV48RS0>
2019-08-13 22:41:43 +0800

    app-bug

--- src/main/java/com/tahoecn/xkc/controller/app/MessageAppController.java
+++ src/main/java/com/tahoecn/xkc/controller/app/MessageAppController.java
@@ -576,6 +576,14 @@ public class MessageAppController extends TahoeBaseController {
 			if("GW".equals(JobCode)){
     			v.setMessageCount(iSystemMessageService.ListDRGJYQOpportunity_SelectCount(map));
     		}else{
+    			sqlWhere = "";
+    			if(!"ZQFZR".equals(JobCode)){//ZQ
+    				sqlWhere += " AND o.SaleUserID = '" + map.get("UserID") + "' ";
+    			}else{//ZQFZR
+    				sqlWhere += " AND EXISTS(SELECT id FROM dbo.B_SalesGroupMember "
+    						+ "WHERE ProjectID='" + map.get("ProjectID") + "'  AND IsDel=0 AND Status=1 AND MemberID=Receiver "
+    						+ "AND RoleID IN('48FC928F-6EB5-4735-BF2B-29B1F591A582', '9584A4B7-F105-44BA-928D-F2FBA2F3B4A4', 'B0BF5636-94AD-4814-BB67-9C1873566F29'))";
+    			}
     			v.setMessageCount(iSystemMessageService.ListDRGJYQClue_SelectCount(map));
     		}
 			v.setContent("");
