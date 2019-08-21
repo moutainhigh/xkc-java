a6901d601fe7db66080cfd0093a09071a480c621
xuzhao <xuzhao@camelot.com>
2019-08-22 07:15:58 +0800

    gaara

---
+++ src/main/java/com/tahoecn/xkc/service/customer/impl/BCustomerpotentialfollowupServiceImpl.java
@@ -0,0 +1,20 @@
+package com.tahoecn.xkc.service.customer.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.tahoecn.xkc.mapper.customer.BCustomerpotentialfollowupMapper;
+import com.tahoecn.xkc.model.clue.BCustomerpotentialfollowup;
+import com.tahoecn.xkc.service.customer.IBCustomerpotentialfollowupService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author YYY
+ * @since 2019-08-22
+ */
+@Service
+public class BCustomerpotentialfollowupServiceImpl extends ServiceImpl<BCustomerpotentialfollowupMapper, BCustomerpotentialfollowup> implements IBCustomerpotentialfollowupService {
+
+}
