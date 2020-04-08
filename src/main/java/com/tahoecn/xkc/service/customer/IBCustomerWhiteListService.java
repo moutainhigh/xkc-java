119302c5886569db38b1c198392547c76f6802fc
chenhong1@tahoecn.com <chenhong1@tahoecn.com>
2020-04-08 09:55:18 +0800

    客户vip白名单--置业顾客客户列表、客户基本信息、客户详细信息

--- src/main/java/com/tahoecn/xkc/service/customer/IBCustomerWhiteListService.java
+++ src/main/java/com/tahoecn/xkc/service/customer/IBCustomerWhiteListService.java
@@ -7,4 +7,11 @@ import com.tahoecn.xkc.model.customer.BCustomerWhiteList;
  * @author mystic
  */
 public interface IBCustomerWhiteListService extends IService<BCustomerWhiteList> {
+
+    /**
+     * 根据顾客ID 判断是否是白名单用户
+     * @param customerID 顾客ID
+     * @return
+     */
+    boolean judgeIsWhiteCustomer(String customerID);
 }
--- src/main/java/com/tahoecn/xkc/service/customer/impl/BCustomerWhiteListServiceImpl.java
+++ src/main/java/com/tahoecn/xkc/service/customer/impl/BCustomerWhiteListServiceImpl.java
@@ -1,5 +1,6 @@
 package com.tahoecn.xkc.service.customer.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tahoecn.xkc.mapper.customer.BCustomerWhiteListMapper;
 import com.tahoecn.xkc.model.customer.BCustomerWhiteList;
@@ -11,4 +12,16 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class BCustomerWhiteListServiceImpl extends ServiceImpl<BCustomerWhiteListMapper, BCustomerWhiteList> implements IBCustomerWhiteListService {
+    @Override
+    public boolean judgeIsWhiteCustomer(String customerID) {
+        QueryWrapper<BCustomerWhiteList> wrapper = new QueryWrapper<>();
+        wrapper.eq("CustomerID", customerID).eq("IsDel", 0);
+        int count = count(wrapper);
+
+        if (count > 0) {
+            return true;
+        }
+
+        return false;
+    }
 }
